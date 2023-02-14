70e1e0164a8055c62afcc94a9d425f24f468dfac
fathomless <digonto.pvt.2021@gmail.com>
2023-02-14 12:20:57 +0530

    LCA

--- src/core/List.hpp
+++ src/core/List.hpp
@@ -3,13 +3,13 @@
 #include <iterator>
 struct ListNode {
   int val;
-  ListNode* next;
+  ListNode *next;
   ListNode() : val(0), next(nullptr) {}
   ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode* next) : val(x), next(next) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-std::ostream& operator<<(std::ostream& os, ListNode* head) {
+std::ostream &operator<<(std::ostream &os, ListNode *head) {
   os << "[";
 
   while (head != nullptr) {
@@ -22,14 +22,15 @@ std::ostream& operator<<(std::ostream& os, ListNode* head) {
   return os;
 }
 
-ListNode* GetList(std::initializer_list<int> l) {
-  ListNode* head = nullptr;
+ListNode *GetList(std::initializer_list<int> l) {
+  ListNode *head = nullptr;
   size_t n = l.size();
 
-  if (n == 0) return head;
+  if (n == 0)
+    return head;
   head = new ListNode(*begin(l));
 
-  ListNode* temp = head;
+  ListNode *temp = head;
   for (auto it = std::next(begin(l)); it != std::end(l); ++it) {
     temp->next = new ListNode(*it);
     temp = temp->next;
